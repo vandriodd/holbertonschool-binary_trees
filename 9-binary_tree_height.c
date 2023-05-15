2d7217d0ccb7e647db3b77090ae8635bdb99b5e9
vandriodd <ludasaadi@gmail.com>
2023-05-15 16:39:18 -0700

    Commit test task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left = 0, right = 0;
 
-	if (!tree || (!tree->left && !tree->right))
+	if (!tree)
 		return (0);
 
 	left = binary_tree_height(tree->left);
