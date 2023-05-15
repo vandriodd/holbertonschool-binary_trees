f629d1b7a78d67696acf9409729cd3c8154a8f85
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-05-15 12:05:17 -0700

    Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,7 +9,7 @@
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    if (tree && tree->parent)
-        return (binary_tree_depth(tree->parent) + 1);
-    return (0);
+	if (tree && tree->parent)
+		return (binary_tree_depth(tree->parent) + 1);
+	return (0);
 }
