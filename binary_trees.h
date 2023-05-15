56d43ca8ebe47fabc10e6498024b5f0a2554f5a7
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-05-15 07:25:09 -0700

    Fix betty header file

--- binary_trees.h
+++ binary_trees.h
@@ -18,16 +18,17 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 
 /* Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
 /* Prototype for test */
 void binary_tree_print(const binary_tree_t *);
