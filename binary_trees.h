a30a6e4eb02dc2ea7ab9c6b48893da10d80ea7b4
vandriodd <ludasaadi@gmail.com>
2023-05-15 17:01:58 -0700

    Add more prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -41,6 +41,9 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int is_it_full(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 /* Prototype for test */
 void binary_tree_print(const binary_tree_t *);
